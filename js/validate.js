a232575fba63b7ef7e685a1252b58271d2f8266a
Jose Manuel Calvo <josema.calvo@servexternos.gruposantander.com>
2020-01-26 19:54:41 +0100

    default

--- js/validate.js
+++ js/validate.js
@@ -8,7 +8,7 @@
 //  }
 //}
 
-function validateField(formfield) {
+function validateUser(formfield) {
   //var x = document.forms["keyForm"]["githubuser"].value;
   var x = formfield;
   var ex = "the filed " + formfield + "is empty";
@@ -19,14 +19,47 @@ function validateField(formfield) {
   }
 }
 
+function validateToken(formfield) {
+  //var x = document.forms["keyForm"]["githubuser"].value;
+  var x = formfield;
+  var ex = "the filed " + formfield + "is empty";
+  
+  if (formfiled == "") {
+    //alert("Name must be filled out");
+    return ex;
+  }
+}
+
+function validateOrg(formfield) {
+  //var x = document.forms["keyForm"]["githubuser"].value;
+  var x = formfield;
+  var ex = "the filed " + formfield + "is empty";
+  
+  if (formfiled == "") {
+    //alert("Name must be filled out");
+    return ex;
+  }
+}
+
+function validateRepo(formfield) {
+  //var x = document.forms["keyForm"]["githubuser"].value;
+  var x = formfield;
+  var ex = "the filed " + formfield + "is empty";
+  
+  if (formfiled == "") {
+    //alert("Name must be filled out");
+    return ex;
+  }
+}
+
 
 
 function validateFormOnSubmit(theForm) {
     var reason = "";
-    reason += validateField(theForm.githubuser);
-    reason += validateField(theForm.githubtoken);
-    reason += validateField(theForm.githuborg);
-    reason += validateField(theForm.githubrepo);
+    reason += validateUser(theForm.githubuser);
+    reason += validateToken(theForm.githubtoken);
+    reason += validateOrg(theForm.githuborg);
+    reason += validateRepo(theForm.githubrepo);
 
     if (reason != "") {
         alert("Some fields need correction:\n" + reason);
