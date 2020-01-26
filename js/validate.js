ac15fef0639b6a3a38df44636f14f15519338e3e
Jose Manuel Calvo <josema.calvo@servexternos.gruposantander.com>
2020-01-26 19:29:38 +0100

    default

--- js/validate.js
+++ js/validate.js
@@ -1,8 +1,37 @@
 
-function validateForm() {
-  var x = document.forms["keyForm"]["githubuser"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
+
+//function validateForm() {
+//  var x = document.forms["keyForm"]["githubuser"].value;
+//  if (x == "") {
+//    alert("Name must be filled out");
+//    return false;
+//  }
+//}
+
+function validateForm(formfield) {
+  //var x = document.forms["keyForm"]["githubuser"].value;
+  var x = formfield;
+  var ex = "the filed " + formfield + "is empty";
+  if (formfiled == "") {
+    //alert("Name must be filled out");
+    return ex;
   }
+}
+
+
+
+function validateFormOnSubmit(theForm) {
+    var reason = "";
+    reason += validateUser(theForm.githubuser);
+    reason += validateToken(theForm.githubtoken);
+    reason += validateOrg(theForm.githuborg);
+    reason += validateRepo(theForm.githubrepo);
+
+    if (reason != "") {
+        alert("Some fields need correction:\n" + reason);
+    } else {
+    	alert("Perfect!!:\n");
+        //simpleCart.checkout();
+    }
+    return false;
 }
